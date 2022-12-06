7989642d932a0b0261600fc412ff379493cd0a0c
piotr-m-jurek <piotr.m.jurek@gmail.com>
2022-12-06 10:49:27 +0100

    day 3 complete

--- rust/src/bin/day-3.rs
+++ rust/src/bin/day-3.rs
@@ -1,32 +1,57 @@
 use anyhow::Error;
 fn main() -> Result<(), Error> {
-    let input: usize = include_str!("day-3.txt")
+    let lines = include_str!("day-3.txt")
         .trim()
-        .lines()
-        .map(|item| {
-            let (first, second) = item.split_at(item.len());
+        .split('\n')
+        .collect::<Vec<&str>>();
 
-            first
-                .chars()
-                .filter(|&char| second.contains(char))
-                .take(1)
-                .next()
-                .unwrap()
+    let task1: usize = lines
+        .iter()
+        .map(|item| {
+            let (first, second) = item.split_at(item.len() / 2);
+
+            get_repeating_letter(first, second)
+                .expect("Get Repeating Letter didn't work at some ponit")
         })
-        .map(get_idx)
+        .map(|letter| get_idx(letter).unwrap())
         .sum();
-    dbg!(input);
+
+    println!("Task 1: {}", task1);
+
+    let task2: usize = lines
+        .chunks(3)
+        .map(|three_lines| {
+            get_repeating_letter2(three_lines[0], three_lines[1], three_lines[2]).unwrap()
+        })
+        .map(|letter| get_idx(letter).unwrap())
+        .sum();
+
+    println!("Task2: {}", task2);
     Ok(())
 }
 
-fn get_idx(letter: char) -> usize {
-    if let Some(idx) = ('a'..='z')
-        .into_iter()
-        .chain(('A'..='Z').into_iter())
-        .position(|c| c == letter)
-    {
-        idx + 1
-    } else {
-        panic!("At the disco")
-    }
+fn get_idx(input: char) -> Option<usize> {
+    let lowercase = ('a'..='z').into_iter();
+    let uppercase = ('A'..='Z').into_iter();
+
+    lowercase
+        .chain(uppercase)
+        .position(|char| char == input)
+        .map(|x| x + 1)
+}
+
+fn get_repeating_letter2(first: &str, second: &str, third: &str) -> Option<char> {
+    first
+        .chars()
+        .filter(|&char| second.contains(char) && third.contains(char))
+        .take(1)
+        .next()
+}
+
+fn get_repeating_letter(first: &str, second: &str) -> Option<char> {
+    first
+        .chars()
+        .filter(|&char| second.contains(char))
+        .take(1)
+        .next()
 }
